1f8c9da9698a9857dee0e19c8947d846c63ed6dd
beom710 <beom710@naver.com>
2025-04-16 00:34:08 +0900

    개발 환경 초기화 및 재설정 -> 완전한 Jetpack Compose 기반

---
+++ README.md
@@ -0,0 +1,15 @@
+# 충북대학교 소프트웨어학과 2025년도 졸업작품 "별도리"
+- Team 천문소프트 
+- 학생 및 천체관측 초보자를 위한 천체관측 교육용 어플리케이션입니다.
+- 구성인원 : 서범수 , 윤태영 , 김채영
+
+## App 구동방법
+1. Gitpull 받은후 android stuido 연결
+2. Andorid studio -> File -> Sync project with Gradle file 실행
+3. 이후 앱 실행
+
+## 개발환경
+- Java 21 
+- Kotlin 1.9.2
+- JetpackCompose 1.5.9
+- 가능하면 개발할때 Project로 보지말고, Android로 편집하기
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,97 +1,55 @@
-import java.io.FileInputStream
-import java.util.Properties
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-}
-
-val localProperties = Properties()
-val localFile = rootProject.file("local.properties")
-if (localFile.exists()) {
-    localProperties.load(FileInputStream(localFile))
 }
 
 android {
-    namespace = "com.example.naver"
-    compileSdk = 35
+    namespace = "com.example.byeoldori"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.naver"
-        minSdk = 24
-        targetSdk = 35
+        applicationId = "com.example.byeoldori"
+        minSdk = 21
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        //API Key를 BuildConfig에 추가
-        buildConfigField("String", "NAVER_CLIENT_ID", "\"${localProperties.getProperty("NAVER_CLIENT_ID")}\"")
-        buildConfigField("String", "NAVER_CLIENT_SECRET", "\"${localProperties.getProperty("NAVER_CLIENT_SECRET")}\"")
-
-        // 네이티브 라이브러리 관련 NDK 필터 추가
-        ndk {
-            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-
-        }
     }
 
-    buildFeatures {
-        buildConfig = true // ✅ BuildConfig 활성화 필요
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
-    implementation("com.naver.maps:map-sdk:3.16.0") //네이버 지도 SDK
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1") //위치 정보 가져오기
-    implementation("com.squareup.retrofit2:retrofit:2.9.0") // Retrofit
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Gson 변환기
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3") // OkHttp 로깅
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4") // Coroutine
+    implementation(platform(libs.compose.bom))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("com.google.android.material:material:1.11.0")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 
 
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
-
