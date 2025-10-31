0a149c30fc9b396f6d9d450c2e7c83faf92c128a
chaeyoung0829 <yong08291@gmail.com>
2025-10-31 16:40:47 +0900

    프로필 UI추가

--- app/src/main/java/com/example/byeoldori/ui/components/mypage/MenuGroupCard.kt
+++ app/src/main/java/com/example/byeoldori/ui/components/mypage/MenuGroupCard.kt
@@ -1,22 +1,12 @@
 package com.example.byeoldori.ui.components.mypage
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ChevronRight
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
